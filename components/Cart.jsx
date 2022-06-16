8ba5258e1af60631fcf077dca1371f175ecdc25e
James Troughton <jamesstroughton@gmail.com>
2022-06-16 22:22:57 +0200

    ✨ Cart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,56 +1,67 @@
-import React from 'react';
-// Cart Item
-//  React Context
+import React from 'react'
+import styles from '../styles/Cart.module.scss'
 
 const CartTest = () => {
-  // Get Cart items
-
-  // Handle Checkout
-  const handleCartCheckout = (e) => {
-    e.preventDefault();
-    // Check if user is logged in
-    // If not, redirect to login page
-    // If yes, redirect to checkout page
-  };
-
-  const handleCartOpen = (e) => {
-    e.preventDefault();
-  };
-
-  let totalPrice = 0;
-  //   let totalQuantity = 0;
-  //   let cartItems = [];
-
   return (
-    <div className='cart'>
-      <div className='cart-labels'>
-        <div className='cart-label-first'>Cart</div>
-        <p>Size</p>
-        <div className='cart-label-last'>
-          <div>Quantity</div>
-          <div className='x-cart' onClick={handleCartOpen}>
-            X
+    <div className={styles.cart}>
+      <div className={styles.cart_inner}>
+        <div className={styles.cart_products}>
+          <div className={styles.cart_products_header}>
+            <div className={styles.cart_products_title}>Cart</div>
+            <div className={styles.cart_products_size}>Size</div>
+            <div className={styles.cart_products_quantity}>Quantity</div>
+            <button className={styles.cart_products_btn}>
+              <svg className='Icon' role='img' viewBox='0 0 50 50'>
+                <g>
+                  <polygon points='50,5 45,0 25,20 5,0 0,5 20,25 0,45 5,50 25,30 45,50 50,45 30,25'></polygon>
+                </g>
+              </svg>
+            </button>
           </div>
+          <ul className={styles.cart_products_list}>
+            <li className={styles.cart_products_list_item}>
+              {/* CART PRODUCTS */}
+            </li>
+          </ul>
         </div>
-      </div>
-      {/* {productsInCart} */}
-      <div className='cart-bottom'>
-        <div className='bottom-first'>
-          <div>Complimentary shipping on all orders.</div>
-          <div>Shipping to the United States</div>
-        </div>
-        <div className='bottom-second'>
-          <div className='sub-total'>
-            <div className='text-sub'>Subtotal [Tax Excl.]</div>
-            <div className='total-price'>${totalPrice}.00</div>
+        <div className={styles.cart_summary}>
+          <div className={styles.cart_summary_wrapper}>
+            <div className={styles.cart_summary_promo}>
+              <button className={styles.cart_summary_promo_btn}>
+                <div className={styles.cart_summary_promo_btn_wrapper}>
+                  <h5 className={styles.cart_summary_promo_label}>
+                    Apply a promotional code.
+                  </h5>
+                  <div className={styles.cart_summary_promo_btn_icon_wrapper}>
+                    <svg
+                      className={styles.cart_summary_promo_btn_icon}
+                      data-ref='promoCodeModal-icon'
+                      role='img'
+                      viewBox='0 0 50 50'
+                    >
+                      <g>
+                        <circle
+                          className={styles.cart_summary_promo_btn_icon_circle}
+                          cx='25'
+                          cy='25'
+                          r='22'
+                          fill='none'
+                        ></circle>
+                        <polygon
+                          className={styles.cart_summary_promo_btn_icon_poly}
+                          points='26.2,15.2 23.8,15.2 23.8,23.9 15,23.9 15,26.4 23.8,26.4 23.8,35.1 26.2,35.1 26.2,26.4 35,26.4 35,23.9 26.2,23.9 '
+                        ></polygon>
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+              </button>
+            </div>
           </div>
-          <button onClick={handleCartCheckout} className='checkout'>
-            Checkout
-          </button>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CartTest;
+export default CartTest
