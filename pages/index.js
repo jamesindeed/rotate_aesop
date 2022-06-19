77a72e6b4b811d3bf2903c9d76d9875130de1145
James Troughton <jamesstroughton@gmail.com>
2022-06-19 10:59:10 +0200

    ✨GSSP

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import styles from '../styles/Main.module.scss'
+import { client } from '../lib/client'
 
-export default function Home() {
+const Home = ({ products }) => {
   return (
     <div className={styles.main}>
       <h1>Rotate Aesop</h1>
+      {products?.map((product) => (
+        <a href='/'>
+          <h3>[ {product.title} ]</h3>
+        </a>
+      ))}
     </div>
   )
+}
+
+export default Home
+
+export const getServerSideProps = async (context) => {
+  const productQuery = '*[_type == "product"] | order(date desc)'
+  const products = await client.fetch(productQuery)
+
+  return {
+    props: { products },
+  }
 }
